f16345845f97dbdbe64bbae056d6fa85bb6510c8
tedd-E <62875128+tedd-E@users.noreply.github.com>
2024-08-04 21:41:40 -0700

    Update Projects table to load items as a card instead of a list

--- src/pages/test-summaries/interfaces.ts
+++ src/pages/test-summaries/interfaces.ts
@@ -1,3 +1,5 @@
-export interface IProjects {
-    project: string
+import {BaseRecord} from "@refinedev/core";
+
+export interface IProject extends BaseRecord {
+    project: string;
 }
--- src/pages/test-summaries/summary.tsx
+++ src/pages/test-summaries/summary.tsx
@@ -1,42 +1,53 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { List, useTable } from "@refinedev/antd";
-import { Table } from "antd";
-import { HttpError } from "@refinedev/core";
+import { Card, Pagination, Row, Col } from "antd";
+import { HttpError, BaseRecord } from "@refinedev/core";
+import {IProject} from "./interfaces";
 
-interface IProject {
-    project: string;
-}
 
 export const TestSummary = () => {
     const { tableProps } = useTable<IProject, HttpError>({
         resource: "projects/",
-
     });
 
-    const projectData = tableProps.dataSource?.map(project => ({
-        project,
-    })) || [];
+    const [currentPage, setCurrentPage] = useState(1);
+    const cardsPerPage = 6;
 
-    const columns = [
-        {
-            title: "Project",
-            dataIndex: "project",
-            key: "project",
-        },
-    ];
+    const projectData: IProject[] = (tableProps.dataSource || []).map((projectName, index) => ({
+        id: index, // need this cause IProject extends Baserecord
+        project: projectName as unknown as string,  //TODO: not good practice, need to fix this
+    }));
+
+    const startIndex = (currentPage - 1) * cardsPerPage; // calculate paginated data
+    const endIndex = startIndex + cardsPerPage;
+    const paginatedData = projectData.slice(startIndex, endIndex);
+
+    const handlePageChange = (page: number) => {
+        setCurrentPage(page);
+    };
 
     return (
-        <List title={"Projects"}>
-            <Table
-                dataSource={projectData}
-                columns={columns}
-                rowKey="project"
+        <List title={"Test Result Overview"}>
+            <Row gutter={16}>
+                {paginatedData.map((project) => (
+                    <Col span={24} key={project.id}>
+                        <Card
+                            hoverable
+                            title={project.project}
+                            style={{ textAlign: 'center', marginBottom: '16px', width: '100%'  }}
+                        >
+                            TODO: put the grid here!
+                        </Card>
+                    </Col>
+                ))}
+            </Row>
+            <Pagination
+                current={currentPage}
+                pageSize={cardsPerPage}
+                total={projectData.length}
+                onChange={handlePageChange}
+                style={{ marginTop: '16px', textAlign: 'center' }}
             />
-                <Table.Column title="Project Name"
-                    dataIndex="project"/>
-
         </List>
     );
 };
-
-export default TestSummary;
